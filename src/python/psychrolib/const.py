c341928c21555efea26a1cf703ec0164fe6cc620
Vahid Naieni <v.naeini@ariabeniz.com>
2019-09-15 12:02:36 +0430

    constants added

---
+++ src/python/psychrolib/const.py
@@ -0,0 +1,54 @@
+#######################################################################################################
+# Global constants
+#######################################################################################################
+
+R_DA_IP = 53.350
+"""float: Universal gas constant for dry air (IP version)
+
+    Units:
+        ft lb_Force lb_DryAir⁻¹ R⁻¹
+
+    Reference:
+        ASHRAE Handbook - Fundamentals (2017) ch. 1
+"""
+
+R_DA_SI = 287.042
+"""float: Universal gas constant for dry air (SI version)
+
+    Units:
+        J kg_DryAir⁻¹ K⁻¹
+
+    Reference:
+        ASHRAE Handbook - Fundamentals (2017) ch. 1
+"""
+
+MAX_ITER_COUNT = 100
+"""int: Maximum number of iterations before exiting while loops.
+
+"""
+
+MIN_HUM_RATIO = 1e-7
+"""float: Minimum acceptable humidity ratio used/returned by any functions.
+          Any value above 0 or below the MIN_HUM_RATIO will be reset to this value.
+
+"""
+
+FREEZING_POINT_WATER_IP = 32.0
+"""float: Freezing point of water in Fahrenheit.
+
+"""
+
+FREEZING_POINT_WATER_SI = 0.0
+"""float: Freezing point of water in Celsius.
+
+"""
+
+TRIPLE_POINT_WATER_IP = 32.018
+"""float: Triple point of water in Fahrenheit.
+
+"""
+
+TRIPLE_POINT_WATER_SI = 0.01
+"""float: Triple point of water in Celsius.
+
+"""
